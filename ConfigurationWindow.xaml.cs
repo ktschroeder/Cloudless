aee9f516c2a67821941836d3128817b836822137
ktschroeder <ktschroeder@wisc.edu>
2024-12-08 02:26:40 -0600

    Cancel button and movable config window

--- ConfigurationWindow.xaml.cs
+++ ConfigurationWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace SimpleImageViewer
 {
@@ -23,6 +26,29 @@ namespace SimpleImageViewer
             SelectedDisplayMode = currentDisplayMode;
         }
 
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // If the left mouse button is pressed and it's not on a clickable element (e.g., buttons or ComboBox)
+            if (e.ChangedButton == MouseButton.Left && !IsControlClicked(e))
+            {
+                // Allow the window to be dragged
+                this.DragMove();
+            }
+        }
+
+        // Helper method to determine if the click is on a clickable control like a button or dropdown
+        private bool IsControlClicked(MouseButtonEventArgs e)
+        {
+            var hit = VisualTreeHelper.HitTest(this, e.GetPosition(this));
+
+            // Check if the hit test is on a Button or ComboBox (any other controls you want to exclude)
+            if (hit?.VisualHit is Button || hit?.VisualHit is ComboBox)
+            {
+                return true;
+            }
+            return false;
+        }
+
         private void Save_Click(object sender, RoutedEventArgs e)
         {
             if (DisplayModeDropdown.SelectedIndex == 0)
@@ -35,6 +61,14 @@ namespace SimpleImageViewer
                 SelectedDisplayMode = "BestFitWithoutZooming";
 
             DialogResult = true;
+            Close();
+        }
+
+
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            //DialogResult = true;
             Close();
         }
     }
