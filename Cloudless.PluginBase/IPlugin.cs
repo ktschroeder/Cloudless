f11635176566ec16d973a68507e3227178a7724b
ktschroeder <ktschroeder@wisc.edu>
2026-05-05 19:56:47 -0500

    Update plugin manifests, framework

--- Cloudless.PluginBase/IPlugin.cs
+++ Cloudless.PluginBase/IPlugin.cs
@@ -6,6 +6,8 @@ namespace Cloudless.PluginBase
     public interface IPlugin
     {
         string Name { get; }
+        string PluginVersion { get; }
+        string MinAppVersion { get; }
         string Description { get; }
         List<string> SupportsFileTypes { get; }
 
